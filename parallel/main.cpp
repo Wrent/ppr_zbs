fb4497924a503e1d79566e61472803a060dbb59e
Adam Kucera <kucerad5@fit.cvut.cz>
2015-12-08 14:51:29 +0100

    pridana prvni verze odesilani grafu.

--- parallel/main.cpp
+++ parallel/main.cpp
@@ -130,7 +130,7 @@ int main(int argc, char * argv[])
 		for (int i = 1; i < p; i++) {
 			//posleme velikost grafu
 			int graphSize = mgraph->size();
-			MPI_Send(graphSize, 1, MPI_INT, i, MSG_GRAPH_SIZE, MPI_COMM_WORLD);
+			MPI_Send(&graphSize, 1, MPI_INT, i, MSG_GRAPH_SIZE, MPI_COMM_WORLD);
 			//posleme samotny graf
 			MPI_Send(&mgraph->front(), mgraph->size(), MPI_CHAR, i, MSG_GRAPH, MPI_COMM_WORLD);
 		}
