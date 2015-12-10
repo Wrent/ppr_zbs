a32d991527f45e650c08b2b1be9d996fc81f03b4
Adam Kucera <kucerad5@fit.cvut.cz>
2015-12-10 18:41:36 +0100

    zmena vectoru na char

--- parallel/assistfunc.h
+++ parallel/assistfunc.h
@@ -10,6 +10,6 @@ using namespace std;
 
 uint64_t gcd(uint64_t x, uint64_t y);
 uint64_t comb(uint64_t n, uint64_t k);
-ifstream& operator>>(ifstream& infile, vector<vector<bool> >& mgraph);
-ostream& operator<<(ostream& os, vector<vector<bool> >& mgraph);
+ifstream& operator>>(ifstream& infile, vector<vector<char> >& mgraph);
+ostream& operator<<(ostream& os, vector<vector<char> >& mgraph);
 ostream& operator<<(ostream& os, const set<uint64_t> *mset);
--- parallel/genlogic.h
+++ parallel/genlogic.h
@@ -9,8 +9,8 @@ using namespace std;
 
 #define _DEBUG
 
-uint64_t priceOfX(vector<vector<bool> >& mgraph, set<uint64_t>& xnodes);
+uint64_t priceOfX(vector<vector<char> >& mgraph, set<uint64_t>& xnodes);
 pair<uint64_t, set<uint64_t>*> workUnit(uint64_t k, uint64_t n,
 										uint64_t *startPrefix, uint64_t *endPrefix,
-										vector<vector<bool> > &mgraph);
-pair<uint64_t, set<uint64_t>*> divideWork(uint64_t k, uint64_t n, vector<vector<bool> > &mgraph);
+										vector<vector<char> > &mgraph);
+pair<uint64_t, set<uint64_t>*> divideWork(uint64_t k, uint64_t n, vector<vector<char> > &mgraph);
