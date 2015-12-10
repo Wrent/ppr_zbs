ba0b09d5fa4dd209aa3f7948197d2fa0c365204b
Adam Kucera <kucerad5@fit.cvut.cz>
2015-12-10 18:46:53 +0100

    zmena vectoru na char

--- parallel/main.cpp
+++ parallel/main.cpp
@@ -135,7 +135,7 @@ int main(int argc, char * argv[])
 			int graphSize = mgraph->size() * mgraph[0].size() * sizeof(char);
 			MPI_Send(&graphSize, 1, MPI_INT, i, MSG_GRAPH_SIZE, MPI_COMM_WORLD);
 			//posleme samotny graf
-			MPI_Send(&mgraph->front(), mgraph->size(), MPI_CHAR, i, MSG_GRAPH, MPI_COMM_WORLD);
+			MPI_Send(&mgraph->front(), graphSize, MPI_CHAR, i, MSG_GRAPH, MPI_COMM_WORLD);
 		}
 
         //vypocet se bude startovat jinak nez tadytim
