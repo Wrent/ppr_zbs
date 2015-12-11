236da3ba445e58f23c275896fbe8880b28c3c640
Adam Kucera <kucerad5@fit.cvut.cz>
2015-12-11 10:23:23 +0100

    rozdelovani prace pomoci komunikace.

--- parallel/main.cpp
+++ parallel/main.cpp
@@ -26,6 +26,7 @@ using namespace std;
 #define MSG_GRAPH		 1005
 #define MSG_GRAPH_SIZE	 1006
 #define MSG_PAR_A        1007
+#define MSG_PAR_N        1008
 
 void printUsage(const char* name)
 {
@@ -95,7 +96,7 @@ void doLocalWorkStep() {
 }
 
 
-uint64_t* getMiddlePrefix(uint64_t* start, uint64_t startSize, uint64_t* end, uint64_t endSize, uint64_t k, uint64_t n) {
+pair<uint64_t, uint64_t*> getMiddlePrefix(uint64_t* start, uint64_t startSize, uint64_t* end, uint64_t endSize, uint64_t k, uint64_t n) {
 	uint64_t diffPos = 0;
 	uint64_t val;
 	while (start[diffPos] == end[diffPos]) {
@@ -117,10 +118,23 @@ uint64_t* getMiddlePrefix(uint64_t* start, uint64_t startSize, uint64_t* end, ui
 	}
 	middle[diffPos] = val;
 
-	for (uint64_t i = 0; i < k; i++) {
-    		cout << middle[i] << " ";
-    	}
-    	cout << endl;
+
+	return pair<uint64_t, uint64_t*>(diffPos + 1, middle);
+}
+
+
+void printPrefixes(uint64_t p, uint64_t *prefix, uint64_t prefixSize, uint64_t *prefixEnd, uint64_t prefixEndSize) {
+    cout << "process " << p << " has:" << endl;
+    cout << "prefix ";
+    for (int i = 0; i < prefixSize; i ++) {
+        cout << prefix[i] << " ";
+    }
+    cout << "of size " << prefixSize << endl;
+    cout << "prefixEnd ";
+    for (int i = 0; i < prefixEndSize; i ++) {
+            cout << prefixEnd[i] << " ";
+        }
+        cout << "of size " << prefixEndSize << endl;
 }
 
 
@@ -130,7 +144,9 @@ int main(int argc, char * argv[])
 	vector<vector<char> > *mgraph = NULL;
 
     uint64_t *prefix, *prefixEnd;
-    uint64_t parA;
+    uint64_t parA. parN;
+    uint64_t prefixSize, prefixEndSize;
+    bool done = true;
 
 	MPI_Status status;
 	MPI_Status recv_status;
@@ -150,10 +166,16 @@ int main(int argc, char * argv[])
 	//nachazime se v ridicim procesu
 	if (my_rank == 0) {
 		parA = strtoull(argv[1], NULL, 10);
-		uint64_t parN = strtoull(argv[2], NULL, 10);
+		parN = strtoull(argv[2], NULL, 10);
 
+        //pripravime prefix
         prefix = new uint64_t[parA];
         prefixEnd = new uint64_t[parA];
+        prefix[0] = 0;
+        prefixEnd[0] = parN - parA;
+        prefixSize = 1;
+        prefixEndSize = 1;
+        done = false;
 
 		mgraph = prepareGraph(argc, argv);
 
@@ -169,17 +191,15 @@ int main(int argc, char * argv[])
 		//odeslat graf
 		for (int i = 1; i < p; i++) {
 		    //posleme parA
-		    MPI_Send(&parA, 1, MPI_INT, i, MSG_PAR_A, MPI_COMM_WORLD);
+		    MPI_Send(&parA, 1, MPI_UNSIGNED_LONG_LONG, i, MSG_PAR_A, MPI_COMM_WORLD);
+            //posleme parN
+    		MPI_Send(&parN, 1, MPI_UNSIGNED_LONG_LONG, i, MSG_PAR_N, MPI_COMM_WORLD);
 
 			//posleme velikost grafu
-			int graphSize = mgraph->size() * mgraph[0].size() * sizeof(char);
-			MPI_Send(&graphSize, 1, MPI_INT, i, MSG_GRAPH_SIZE, MPI_COMM_WORLD);
+			uint64_t graphSize = mgraph->size() * mgraph[0].size() * sizeof(char);
+			MPI_Send(&graphSize, 1, MPI_UNSIGNED_LONG_LONG, i, MSG_GRAPH_SIZE, MPI_COMM_WORLD);
 			//posleme samotny graf
 			MPI_Send(&mgraph->front(), graphSize, MPI_CHAR, i, MSG_GRAPH, MPI_COMM_WORLD);
-
-
-
-			//posleme praci
 		}
 
         //vypocet se bude startovat jinak nez tadytim
@@ -191,26 +211,28 @@ int main(int argc, char * argv[])
 	    //tady si ostatni procesory prijmou praci rozeslanou prvnim procesorem
 	    //take by to slo tuhle cast vynechat, ze prvni procesor proste zacne pracovat, dostane zadosti o praci a ty vyridi
         //prijmout parA
-        MPI_Recv ( &parA, 1, MPI_INT, 0, MSG_PAR_A, MPI_COMM_WORLD, &status);
+        MPI_Recv ( &parA, 1, MPI_UNSIGNED_LONG_LONG, 0, MSG_PAR_A, MPI_COMM_WORLD, &status);
+        //prijmout parN
+        MPI_Recv ( &parN, 1, MPI_UNSIGNED_LONG_LONG, 0, MSG_PAR_N, MPI_COMM_WORLD, &status);
 
         prefix = new uint64_t[parA];
         prefixEnd = new uint64_t[parA];
+        prefixSize = 0;
+        prefixEndSize = 0;
 
         cout << p << " received parA " << parA << endl;
-        int graphSize = 0;
+        cout << p << " received parN " << parN << endl;
+        uint64_t graphSize = 0;
         //prijmout velikost grafu
-        MPI_Recv ( &graphSize, 1, MPI_INT, 0, MSG_GRAPH_SIZE, MPI_COMM_WORLD, &status);
+        MPI_Recv ( &graphSize, 1, MPI_UNSIGNED_LONG_LONG, 0, MSG_GRAPH_SIZE, MPI_COMM_WORLD, &status);
         cout << p << " is receiving graph of size " << graphSize << endl;
 	    //prijmout graf
 	    MPI_Recv ( &mgraph, graphSize, MPI_CHAR, 0, MSG_GRAPH, MPI_COMM_WORLD, &status);
 	    cout << p << " received graph." << endl;
-	    cout << "graph " << *mgraph << endl;
+	    //cout << "graph " << *mgraph << endl;
 	}
-	MPI_Finalize();
-	return 0;
 
     int recv;
-    bool done = false;
 	//hlavni pracovni smycka
     while (true) {
         i++;
@@ -221,75 +243,95 @@ int main(int argc, char * argv[])
                 //v promenne status je tag (status.MPI_TAG), cislo odesilatele (status.MPI_SOURCE)
                 //a pripadne cislo chyby (status.MPI_ERROR)
                 switch (status.MPI_TAG) {
-                    case MSG_WORK_REQUEST :
-                                            //prijmeme zpravu
-                                            MPI_Recv(&recv, 1, MPI_INT, status.MPI_SOURCE, MSG_WORK_REQUEST, MPI_COMM_WORLD, &recv_status);
-                                            if (localWorkExists()) {
-                                                //rozdelime si svou praci a pulku posleme procesoru
-                                                //rozdelPraci()
-                                                //MPI_Send(prace, velikost prace, MPI_CHAR, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD);
-                                            } else {
-                                                //zadnou praci nemam
-                                                MPI_Send(0, 1, MPI_INT, status.MPI_SOURCE, MSG_WORK_NOWORK, MPI_COMM_WORLD);
-                                            }
-                                            break;
-                    case MSG_WORK_SENT :    // prisel rozdeleny zasobnik, prijmout
-                                            // deserializovat a spustit vypocet
-                                            //MPI_Recv(&prace, velikost prace, MPI_CHAR, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD, &recv_status);
-                                            //zpracuj a pokracuj v pocitani
-                                            done = false;
-                                            break;
-                    case MSG_WORK_NOWORK :  // odmitnuti zadosti o praci
-                                            MPI_Recv(&recv, 1, MPI_INT, status.MPI_SOURCE, MSG_WORK_REQUEST, MPI_COMM_WORLD, &recv_status);
-                                            // zkusit jiny proces
-                                            askForWorkFrom = (askForWorkFrom) % p;
-                                            if (askForWorkFrom == my_rank) {
-                                                done = true;
-                                            } else {
-                                                MPI_Send(0, 1, MPI_INT, askForWorkFrom, MSG_WORK_REQUEST, MPI_COMM_WORLD);
-                                            }
-                                            break;
-                    case MSG_TOKEN :        //ukoncovaci token, prijmout a nasledne preposlat
-                                            MPI_Recv(&recv, 1, MPI_INT, status.MPI_SOURCE, MSG_TOKEN, MPI_COMM_WORLD, &recv_status);
-                                            recv = recv && done;
-                                            if (my_rank == 0) {
-                                                if (recv == 1) {
-                                                    //vsichni jsou hotovy
-                                                    //odesli finish token
-                                                    for (int i = 1; i < p; i++) {
-                                                        MPI_Send(0, 1, MPI_INT, i, MSG_FINISH, MPI_COMM_WORLD);
-                                                    }
-                                                    //min = reseni nalezene procesem 0;
+                            case MSG_WORK_REQUEST :
+                                                    //prijmeme zpravu
+                                                    MPI_Recv(&recv, 1, MPI_INT, status.MPI_SOURCE, MSG_WORK_REQUEST, MPI_COMM_WORLD, &recv_status);
+                                                    if (localWorkExists()) {
+                                                        //rozdelime si svou praci a pulku posleme procesoru
+                                                        //TODO sem se musi posilat jako start Prefix current Prefix
+                                                        pair<uint64_t, uint64_t*> divided = getMiddlePrefix(prefix, prefixSize, prefixEnd, prefixEndSize, parA, parN);
 
-                                                    //a prijmi vysledek
-                                                    for (int i = 1; i < p; i++) {
-                                                        //MPI_Recv(recvMin, velikost reseni, MPI_CHAR, i, MSG_FINISH, MPI_COMM_WORLD, &recv_status);
-                                                        // if (recvMin < min) {
-                                                        //      min = recvMin;
-                                                        // }
+                                                        uint64_t *newPrefix, *newPrefixEnd;
+                                                        uint64_t newPrefixSize, newPrefixEndSize;
+                                                        newPrefixEnd = prefixEnd;
+                                                        newPrefixEndSize = prefixEndSize;
+                                                        newPrefix = divided.second;
+                                                        newPrefixSize = divided.first;
+                                                        prefixEnd = newPrefix;
+                                                        prefixEndSize = newPrefixSize;
 
+                                                        printPrefixes(my_rank, prefix, prefixSize, prefixEnd, prefixEndSize);
+
+                                                        MPI_Send(newPrefixSize, 1, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD);
+                                                        MPI_Send(newPrefixEndSize, 1, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD);
+                                                        MPI_Send(newPrefix, parA, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD);
+                                                        MPI_Send(newPrefixEnd, parA, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD);
+                                                    } else {
+                                                        //zadnou praci nemam
+                                                        MPI_Send(0, 1, MPI_INT, status.MPI_SOURCE, MSG_WORK_NOWORK, MPI_COMM_WORLD);
                                                     }
-                                                    //vypis vysledek a ukonci se
-                                                    // vypis()
+                                                    break;
+                            case MSG_WORK_SENT :    // prisel rozdeleny zasobnik, prijmout
+                                                    // deserializovat a spustit vypocet
+                                                    MPI_Recv(&prefixSize, 1, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD, &recv_status);
+                                                    MPI_Recv(&prefixEndSize, 1, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD, &recv_status);
+                                                    MPI_Recv(&prefix, parA, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD, &recv_status);
+                                                    MPI_Recv(&prefixEnd, parA, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD, &recv_status);
+
+                                                    printPrefixes(my_rank, prefix, prefixSize, prefixEnd, prefixEndSize);
+                                                    done = false;
+                                                    break;
+                            case MSG_WORK_NOWORK :  // odmitnuti zadosti o praci
+                                                    MPI_Recv(&recv, 1, MPI_INT, status.MPI_SOURCE, MSG_WORK_REQUEST, MPI_COMM_WORLD, &recv_status);
+                                                    // zkusit jiny proces
+                                                    askForWorkFrom = (askForWorkFrom) % p;
+                                                    if (askForWorkFrom == my_rank) {
+                                                        done = true;
+                                                    } else {
+                                                        MPI_Send(0, 1, MPI_INT, askForWorkFrom, MSG_WORK_REQUEST, MPI_COMM_WORLD);
+                                                    }
+                                                    break;
+                            case MSG_TOKEN :        //ukoncovaci token, prijmout a nasledne preposlat
+                                                    MPI_Recv(&recv, 1, MPI_INT, status.MPI_SOURCE, MSG_TOKEN, MPI_COMM_WORLD, &recv_status);
+                                                    recv = recv && done;
+                                                    if (my_rank == 0) {
+                                                        if (recv == 1) {
+                                                            //vsichni jsou hotovy
+                                                            //odesli finish token
+                                                            for (int i = 1; i < p; i++) {
+                                                                MPI_Send(0, 1, MPI_INT, i, MSG_FINISH, MPI_COMM_WORLD);
+                                                            }
+                                                            //min = reseni nalezene procesem 0;
+
+                                                            //a prijmi vysledek
+                                                            for (int i = 1; i < p; i++) {
+                                                                //MPI_Recv(recvMin, velikost reseni, MPI_CHAR, i, MSG_FINISH, MPI_COMM_WORLD, &recv_status);
+                                                                // if (recvMin < min) {
+                                                                //      min = recvMin;
+                                                                // }
+
+                                                            }
+                                                            //vypis vysledek a ukonci se
+                                                            // vypis()
+                                                            MPI_Finalize();
+                                                            exit (0);
+                                                        }
+                                                    } else {
+                                                        MPI_Send(&recv, 1, MPI_INT, (my_rank + 1) % p, MSG_TOKEN, MPI_COMM_WORLD);
+                                                    }
+                                                    break;
+                            case MSG_FINISH :
+                                                    if (my_rank != 0) {
+                                                        MPI_Recv(&recv, 1, MPI_INT, 0, MSG_FINISH, MPI_COMM_WORLD, &recv_status);
+                                                        //MPI_Send(mojeReseni, velikost reseni, MPI_CHAR, 0, MSG_FINISH, MPI_COMM_WORLD);
+                                                        //jestlize se meri cas, nezapomen zavolat koncovou barieru MPI_Barrier (MPI_COMM_WORLD)
+                                                    }
                                                     MPI_Finalize();
                                                     exit (0);
-                                                }
-                                            } else {
-                                                MPI_Send(&recv, 1, MPI_INT, (my_rank + 1) % p, MSG_TOKEN, MPI_COMM_WORLD);
-                                            }
-                                            break;
-                    case MSG_FINISH :
-                                            if (my_rank != 0) {
-                                                MPI_Recv(&recv, 1, MPI_INT, 0, MSG_FINISH, MPI_COMM_WORLD, &recv_status);
-                                                //MPI_Send(mojeReseni, velikost reseni, MPI_CHAR, 0, MSG_FINISH, MPI_COMM_WORLD);
-                                                //jestlize se meri cas, nezapomen zavolat koncovou barieru MPI_Barrier (MPI_COMM_WORLD)
-                                            }
-                                            MPI_Finalize();
-                                            exit (0);
-                                            break;
+                                                    break;
 
-                    default :               //error;
-                                            break;
+                            default :               //error;
+                                                    break;
                 }
             }
 
