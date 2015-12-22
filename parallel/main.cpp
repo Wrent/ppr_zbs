fecedf408312c8bce2a9908aef879e1df5ef731d
Petr <remaerc@gmail.com>
2015-12-22 01:10:50 +0100

    debug

--- parallel/main.cpp
+++ parallel/main.cpp
@@ -264,12 +264,12 @@ int main(int argc, char * argv[])
                                                             for (uint64_t i = 0; i < newPrefixSize; i ++) {
                                                                 std::cout << newPrefix[i] << (i!=(parA-1)? ",":"}\n");
                                                             }
-                                                            std::cout << "of size " << newPrefixSize << "to: " << status.MPI_SOURCE<< std::endl;
+                                                            std::cout << "of size " << newPrefixSize << " to: " << status.MPI_SOURCE<< std::endl;
                                                             std::cout << "["<<my_rank << "]prefixEnd ";
                                                             for (uint64_t i = 0; i < newPrefixEndSize; i ++) {
                                                                     std::cout << newPrefixEnd[i] << (i!=(parA-1)? ",":"}\n");
                                                                 }
-                                                                std::cout << "of size " << newPrefixEndSize << "to: " << status.MPI_SOURCE<< std::endl;
+                                                                std::cout << "of size " << newPrefixEndSize << " to: " << status.MPI_SOURCE<< std::endl;
                                                         //#endif
                                                         MPI_Send(&newPrefixSize, 1, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD);
                                                         MPI_Send(&newPrefixEndSize, 1, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, MSG_WORK_SENT, MPI_COMM_WORLD);
